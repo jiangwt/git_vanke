8560857ded77840602e82dfde7ad990b64daf5ce
jiangwt80 <jiangwt80@qq.com>
2016-11-09 23:59:20 +0800

    luoh_update

--- src/com/master/Master.java
+++ src/com/master/Master.java
@@ -5,7 +5,7 @@ public class Master {
 	public static void main(String[] args) {
 
 		System.out.println("master_1");
-		System.out.println("master_add_jwt");
+		System.out.println("master_add_luohua_update");
 		System.out.println("new_project");
 		
 		System.out.println("new_fn_luoh");
