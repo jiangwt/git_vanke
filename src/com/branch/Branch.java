200301312af71d40d248985ab36bef3d69435d8f
姜文天 <jiangwt02@vanke.com>
2018-12-29 10:10:39 +0800

    Revert "remove bransh"
    This reverts commit ea08888f428b18e4c5d6be1237522fa64d4d5cae.

--- src/com/branch/Branch.java
+++ src/com/branch/Branch.java
@@ -3,7 +3,13 @@ package com.branch;
 public class Branch {
 	
 	public static void main(String[] args) {
-	
+		System.out.println("branch_1");
+		System.out.println("branch_add_jwt");
+		
+		System.out.println("branch 2018 12yue ");
+		
+		
+		System.out.println("branch2018  12jjjjj");
 	}
 
 }
